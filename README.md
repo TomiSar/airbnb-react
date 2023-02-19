084206047c138244f7e6683fe9ec88e9271cd933
Tomi Sarjamo <tomppa.sarjamo@gmail.com>
2023-02-19 23:05:57 +0200

    Implemented the Date & Calendar Functionality, Search Functionality & Page, Card component and Loading bar.

--- README.md
+++ README.md
@@ -1,36 +1,24 @@
 # ReactJs Airbnb clone application
+
 ```
 This app is Airbnb clone application made with NextJS
 ```
 
 # Deployed application
+
 ```
+https://airbnb-react-tomisar.vercel.app/
 ```
 
 ## Create React project
+
 ```
 npx create-next-app -e with-tailwindcss airbnb
 ```
 
 ## Project steps
+
 ```
-Creating the Header Component
-Building the Left Side of the Header
-Building the Search Bar (1/2)
-Implementing Heroicons
-Building the Search Bar (2/2)
-Building the Right Side of the Header
-Building the Banner Component
-Explaining Server Side Rendering in Next.js
-Implementing Server Side Rendering
-Creating the Small Card Component                   
-Building the Left Side of the Small Card Component
-Building the Right Side of the Small Card Component
-Building the Medium Card Component
-Hiding the Scrollbar using Tailwind CSS
-Building the Large Card Component
-Building the Footer Component
-Deploying to Vercel (1/2)
 Implementing the Date & Calendar Functionality
 Building the Search Functionality & Page
 Building the Info Card Component
@@ -40,6 +28,7 @@ Deploying to Vercel (2/2)
 ```
 
 ## Stack and framework used
+
 ```
 React
 NextJs
@@ -63,10 +52,14 @@ Next Auth authentication
 ```
 
 ## Installation of needed Packages for application uging npm or yarn
+
 ```
 npm install -D tailwindcss postcss autoprefixer
 npm install @heroicons/react@v1
 npm install tailwind-scrollbar-hide
+npm install --save react-date-range
+npm install --save react date-fns
+npm install @badrap/bar-of-progress
 yarn add next-auth@4.3.4
 npm install -g stepzen
 yarn add @apollo/client graphql
@@ -78,15 +71,17 @@ yarn add @uiball/loaders
 ```
 
 # Next.js + Tailwind CSS Example
+
 ```
-This example shows how to use [Tailwind CSS](https://tailwindcss.com/) 
-[(v3.2)] (https://tailwindcss.com/blog/tailwindcss-v3-2) with Next.js. 
+This example shows how to use [Tailwind CSS](https://tailwindcss.com/)
+[(v3.2)] (https://tailwindcss.com/blog/tailwindcss-v3-2) with Next.js.
 It follows the steps outlined in the official [Tailwind docs](https://tailwindcss.com/docs/guides/nextjs).
 ```
 
 ## Deploy your own
+
 ```
-Deploy the example using [Vercel](https://vercel.com?utm_source=github&utm_medium=readme&utm_campaign=next-example) 
+Deploy the example using [Vercel](https://vercel.com?utm_source=github&utm_medium=readme&utm_campaign=next-example)
 or preview live with [StackBlitz](https://stackblitz.com/github/vercel/next.js/tree/canary/examples/with-tailwindcss)
 
 
@@ -94,9 +89,10 @@ or preview live with [StackBlitz](https://stackblitz.com/github/vercel/next.js/t
 ```
 
 ## How to use
+
 ```
-Execute [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app) 
-with [npm](https://docs.npmjs.com/cli/init), [Yarn](https://yarnpkg.com/lang/en/docs/cli/create/), 
+Execute [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app)
+with [npm](https://docs.npmjs.com/cli/init), [Yarn](https://yarnpkg.com/lang/en/docs/cli/create/),
 or [pnpm](https://pnpm.io) to bootstrap the example:
 ```
 
@@ -113,6 +109,6 @@ pnpm create next-app --example with-tailwindcss with-tailwindcss-app
 ```
 
 ```
-Deploy it to the cloud with [Vercel](https://vercel.com/new?utm_source=github&utm_medium=readme&utm_campaign=next-example) 
+Deploy it to the cloud with [Vercel](https://vercel.com/new?utm_source=github&utm_medium=readme&utm_campaign=next-example)
 ([Documentation](https://nextjs.org/docs/deployment)).
 ```
--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,59 @@
+import { useState } from "react";
 import Image from "next/image";
 import {
   GlobeAltIcon,
   MenuIcon,
   SearchIcon,
   UserCircleIcon,
+  UsersIcon,
 } from "@heroicons/react/solid";
+import { DateRangePicker } from "react-date-range";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { useRouter } from "next/router";
 
-function Header() {
+function Header({ placeHolder }) {
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [numberOfGuests, setNumberOfGuests] = useState(1);
+  const router = useRouter();
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const resetInput = () => {
+    setSearchInput("");
+  };
+
+  const search = () => {
+    // router.push("/search");
+    router.push({
+      pathname: "/search",
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        numberOfGuests,
+      },
+    });
+  };
+
   return (
     <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
       {/* left */}
-      <div className='relative flex items-center h-10 cursor-pointer my-auto'>
+      <div
+        className='relative flex items-center h-10 cursor-pointer my-auto'
+        onClick={() => router.push("/")}
+      >
         <Image
           src='https://static.dezeen.com/uploads/2014/07/Airbnb-rebrand-by-DesignStudio_dezeen_468_8.jpg'
           alt='header'
@@ -25,7 +68,9 @@ function Header() {
         <input
           className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400'
           type='text'
-          placeholder='Start your search...'
+          placeholder={placeHolder || "Start your search..."}
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
         />
         <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2' />
       </div>
@@ -40,6 +85,40 @@ function Header() {
           <UserCircleIcon className='h-6' />
         </div>
       </div>
+      {searchInput && (
+        <div className='flex flex-col col-span-3 mx-auto mt-5'>
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FF5A5F"]}
+            onChange={handleSelect}
+          />
+          <div className='flex items-center border-b mb-4'>
+            <h2 className='text-2xl flex-grow font-semibold'>
+              Number of Guests
+            </h2>
+            <UsersIcon className='h-5' />
+            <input
+              className='w-12 pl-2 text-lg outline-none text-red-400'
+              type='number'
+              value={numberOfGuests}
+              onChange={(e) => setNumberOfGuests(e.target.value)}
+              min={1}
+            />
+          </div>
+          <div className='flex'>
+            <button
+              className='flex-grow text-gray-500 gray-500'
+              onClick={resetInput}
+            >
+              Cancel
+            </button>
+            <button className='flex-grow text-red-400' onClick={search}>
+              Search
+            </button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
